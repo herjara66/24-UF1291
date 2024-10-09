b8270d0490467dbac24ae047debfff8da2d5d91a
herjara66 <jaramillojesush@gmail.com>
2024-10-09 11:34:58 +0200

    fix

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 // app.js
-const express = require('express');
-const path = require('path');
-const connection = require('./config');
+const express = require("express");
+const path = require("path");
+const connection = require("./config");
 const app = express();
 const PORT = process.env.APP_PORT;
 
 // Servir archivos estáticos (HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Endpoint para buscar estudiantes
-app.get('/student/:id', (req, res) => {
+app.get("/student/:id", (req, res) => {
   const studentId = req.params.id;
-  const query = 'SELECT * FROM students WHERE id = ?';
+  const query = "SELECT * FROM students WHERE id = ?";
 
   connection.query(query, [studentId], (err, results) => {
     if (err) {
-      res.status(500).json({ error: 'Error fetching student data' });
+      res.status(500).json({ error: "Error fetching student data" });
       return;
     }
     if (results.length === 0) {
-      res.status(404).json({ error: 'Student not found' });
+      res.status(404).json({ error: "Student not found" });
       return;
     }
     const student = results[0];
